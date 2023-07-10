393a525ca83c6c4f7df7a385272d5cf83af99148
w41k4z <alainricor@gmail.com>
2023-07-10 21:11:46 +0300

    database script fixing

--- application/controllers/DietController.php
+++ application/controllers/DietController.php
@@ -3,11 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class DietController extends CI_Controller
 {
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     $this->load->model('DietModel');
-    // }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('DietModel');
+    }
 
     private function viewer($page, $data)
     {
--- script/Database_sakafo.sql
+++ script/Database_sakafo.sql
@@ -1,27 +1,46 @@
-CREATE TABLE regime(
-	idRegime  SERIAL PRIMARY KEY,
-    nombreRepas int,
+CREATE TABLE activite (
+    id SERIAL PRIMARY KEY,
+    niveau INT NOT NULL, -- < 5 facile, < 10 moyen, < 20 difficile
+    CHECK(niveau > 0)
+);
+
+CREATE TABLE detail_activite (
+    id SERIAL PRIMARY KEY,
+    id_activite INT REFERENCES activite(id) NOT NULL, 
+    activite VARCHAR(30) NOT NULL, -- ex: Pompe 5
+    UNIQUE(id_activite, activite)
+);
+
+CREATE TABLE categorie_regime (
+    id SERIAL PRIMARY KEY,
+    categorie VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE regime (
+	id SERIAL PRIMARY KEY,
 	nom VARCHAR(15) NOT NULL,
-    check(nombreRepas>0)
+    id_categorie INT REFERENCES categorie_regime(id) NOT NULL,
+    duree INT NOT NULL, -- unite hebdomadaire
+    de INT NOT NULL, -- borne inferieur
+    a INT NOT NULL, -- borne superieur
+    prix DOUBLE PRECISION NOT NULL, -- prix unitaire/j
+    CHECK(duree > 0),
+    CHECK(de > 0 AND de < a),
+    CHECK(prix > 0)
 );
 
-CREATE TABLE composant(
-    idComposant  SERIAL PRIMARY KEY,
-    nomComposant VARCHAR(15) not null unique
+CREATE TABLE plat (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL UNIQUE,
+    etat INT NOT NULL, -- 0 matin, 5 midi, 10 soir
+    CHECK(etat >= 0)
 );
 
-CREATE TABLE detail_regime(
-    idDetailRegime SERIAL PRIMARY key,
-    idComposant int not null,
-    idRegime int not null,
-    foreign key (idComposant) references composant(idComposant),
-    foreign key (idRegime) references regime(idRegime)
-);
-
-CREATE TABLE tarification(
-    idRegime  serial PRIMARY key,
-    prix double precision not null,
-    dure int not null,
-    poids double precision not null,
-    foreign key (idRegime) references regime(idRegime)
+CREATE TABLE detail_regime (
+    id SERIAL PRIMARY KEY,
+    jour INT NOT NULL, -- ex: Jour 1
+    id_plat_matin INT REFERENCES plat(id) NOT NULL,
+    id_plat_midi INT REFERENCES plat(id) NOT NULL,
+    id_plat_soir INT REFERENCES plat(id) NOT NULL,
+    id_activite INT REFERENCES activite(id) NOT NULL
 );
